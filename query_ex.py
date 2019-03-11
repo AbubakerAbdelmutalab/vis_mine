8c9bee276913c586af370f2aef938ad38c9afe40
turbosheep <10606045+turbosheep@users.noreply.github.com>
2019-03-11 13:49:14 -0600

    preliminary experiments with versions

--- query_ex.py
+++ query_ex.py
@@ -9,8 +9,6 @@ url = 'https://kibana.emulab.net/elasticsearch/_all/_search'
 h = {'content-type':'application/json'}
 loaded_query = open("query.json")
 json_query = loaded_query.read()
-user = "abubaker"
-password ="BDf%tJ#VS@6JrAw@9Ktc"
 response = requests.post(url, data = json_query,headers=h, auth=get_connection())
 if (response.status_code == 401):
 	print("HTTP error 401: Client Error, possibly due to incorrect credentials")
@@ -21,13 +19,20 @@ else:
 json_response = json.loads(response.text)
 hits = json_response['hits']['hits']
 file=open('file','a')
+time = []
+version = []
+filter_version = []
+print(len(hits))
 for hit in hits:
 	log_entry= hit['_source']
-	time = log_entry['timestamp']
+	time.append(log_entry['@timestamp'])
 	message = log_entry['message']
-	version = log_entry['@version']
-	print(message)
-	file.write(message)
-file.close()
-plt.bar(version, time)
-plt.show()
+	version.append(log_entry['@version'])
+	filter_version.append(log_entry['logstash_filter_version'])
+	#print(message)
+	#file.write(message)
+#file.close()
+plt.hist(filter_version)
+plt.hist(version)
+plt.savefig("version_hist.png")
+
