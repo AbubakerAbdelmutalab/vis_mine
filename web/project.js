a9040a9da2642f465ca8727990a16e2cb0d45171
connor <connorotte@gmail.com>
2019-04-16 15:04:15 -0600

    finished functionality and styling

--- web/project.js
+++ web/project.js
@@ -11,26 +11,39 @@ var map = new Datamap({
         red: 'red'
     }
 });
+var all = true;
+var types = ["INVALID_USER", "FAILED_PASSWORD", "DISCONNECT", "DISCONNECT_INVALID_USER", "ACCEPT_PUBLIC_KEY", "DISCONNECT_PREAUTH", "REVERSE_MAPPING", "TOO_MANY_PASSWORD_ATTEMPTS", "UNABLE_TO_NEGOTIATE", "NEW_SESSION", "REMOVE_SESSION", "NO_IDENTIFICATION"];
+var checkboxes = [];
+types.forEach((type) => {
+    checkboxes.push(document.getElementById(type));
+});
 var data = JSON.parse(data); //import the data included in the html file
 var slider = document.getElementById("myRange");
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
+    boxChecked();
+}
+
+function checkAll() {
+    var allBox = document.getElementById("ALL");
+    checkboxes.forEach((box, index) => {
+        box.checked = allBox.checked;
+    });
     boxChecked();
 }
 function boxChecked() {
-    map.bubbles([]);
-    var types = ["INVALID_USER", "FAILED_PASSWORD", "DISCONNECT", "DISCONNECT_INVALID_USER", "ACCEPT_PUBLIC_KEY", "DISCONNECT_PREAUTH", "REVERSE_MAPPING", "TOO_MANY_PASSWORD_ATTEMPTS", "UNABLE_TO_NEGOTIATE", "NEW_SESSION", "REMOVE_SESSION", "NO_IDENTIFICATION"];
-    var checkboxes = [];
-    types.forEach((type) => {
-        checkboxes.push(document.getElementById(type));
-    });
     var checked = [];
     checkboxes.forEach((box, index) => {
-        if(box.checked == true){
+        if (box.checked == true) {
             checked.push(types[index]);
         }
     });
-    var bubbles = getBubbles(checked, slider.value);
+    var bubbles = [];
+    if(all){
+        bubbles = getBubbles(checked, false);
+    } else{
+        bubbles = getBubbles(checked, slider.value);
+    }
     bubbles = bubbles.sort((a, b) => { return b.loginAttempts - a.loginAttempts });
     map.bubbles(bubbles, {
         popupTemplate: function (geo, data) {
@@ -39,36 +52,54 @@ function boxChecked() {
     });
 }
 
-function getBubbles(types,section) {
-	var i = Math.floor((section-1)*(data.length/10));
-    var max = Math.floor(i + (data.length/10));
-	var countryCount = new Map()
-	while (i < max) {
-		var msg = data[i]
-		if (types.indexOf(msg.type) >= 0 && !(msg.country === "UNKNOWN")) {
-			//if country is not yet mapped, create a new entry
-		    if (countryCount.get(data[i].country) == null) {
-		        countryCount.set(data[i].country, { count: 0, latitude: data[i].latitude, longitude: data[i].longitude });
-		    }
-		    //increment the count and add to map
-		    let countryStuff = countryCount.get(data[i].country)
-		    countryStuff.count++;
-		    countryCount.set(data[i].country, countryStuff);
-		}
-		i++
-	}
-
-	var bubbles = [];
-	countryCount.forEach(function (value, key) {
-    bubble = {
-        radius: 5+value.count*.1, //increase the radius of the bubble for every login attempt by the country
-        loginAttempts: value.count,
-        country: key,
-        latitude: value.latitude,
-        longitude: value.longitude,
-        fillKey: 'red',
+function getBubbles(types, section) {
+    var i = 0;
+    var max = data.length;
+    if(section){
+        i = Math.floor((section - 1) * (data.length / 10));
+        max = Math.floor(i + (data.length / 10));
     }
-    bubbles.push(bubble);
-	});
-	return bubbles
+    var countryCount = new Map()
+    while (i < max) {
+        var msg = data[i]
+        if (types.indexOf(msg.type) >= 0 && !(msg.country === "UNKNOWN")) {
+            //if country is not yet mapped, create a new entry
+            if (countryCount.get(data[i].country) == null) {
+                countryCount.set(data[i].country, { count: 0, latitude: data[i].latitude, longitude: data[i].longitude });
+            }
+            //increment the count and add to map
+            let countryStuff = countryCount.get(data[i].country)
+            countryStuff.count++;
+            countryCount.set(data[i].country, countryStuff);
+        }
+        i++
+    }
+
+    var bubbles = [];
+    countryCount.forEach(function (value, key) {
+        bubble = {
+            radius: 5 + value.count * .1, //increase the radius of the bubble for every login attempt by the country
+            loginAttempts: value.count,
+            country: key,
+            latitude: value.latitude,
+            longitude: value.longitude,
+            fillKey: 'red',
+        }
+        bubbles.push(bubble);
+    });
+    return bubbles
+}
+
+function allTime(){
+    all = !all;
+    var slider = document.getElementById("myRange");
+    var button = document.getElementById("timeButton");
+    if(all){
+        slider.style.visibility = "hidden";
+        button.innerText = "View Data by Time Period";
+    } else {
+        slider.style.visibility = "visible";
+        button.innerText = "View Data For All Time";
+    }
+    boxChecked();
 }
